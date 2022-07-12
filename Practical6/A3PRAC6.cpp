54c322ab9ca05d104d589170d8df40ff63e6bd77
Avaneesh-Chopdekar <89315115+Avaneesh-Chopdekar@users.noreply.github.com>
2022-07-12 19:01:41 +0530

    Added Aim for Practical 6

--- Practical6/A3PRAC6.cpp
+++ Practical6/A3PRAC6.cpp
@@ -1,6 +1,14 @@
 #include<iostream.h>
 #include<conio.h>
 
+/*
+    Aim:
+    Write a program in C++ that first initializes an array of 5 given numbers.
+    The program must add these numbers by traversing this array with a pointer.
+    The output should print starting address of array and size of number to which it points.
+    The program must also print the sum and pointer address with the addition of every number as well as the ending address.
+*/
+
 void main()
 {
     float a[] = {10.2, 3.9, 4.6, 5.5, 6.9};
