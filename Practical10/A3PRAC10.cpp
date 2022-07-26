03d03ff0ff2bdb4a9cd099ddb61fafa46978884b
Avaneesh-Chopdekar <89315115+Avaneesh-Chopdekar@users.noreply.github.com>
2022-07-26 19:26:22 +0530

    Added Practical 10

---
+++ Practical10/A3PRAC10.cpp
@@ -0,0 +1,52 @@
+#include <iostream.h>
+#include <conio.h>
+
+//  Aim: Write a c++ program for binary search.
+
+void main()
+{
+    clrscr();
+
+    int n, i, arr[50], search, first, last, middle;
+
+    cout << "Enter total number to elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " numbers: ";
+    for(i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter a number to find: ";
+    cin >> search;
+
+    first = 0;
+    last = n - 1;
+    middle = (first + last) / 2;
+
+    while (first <= last)
+    {
+        if (arr[middle] < search)
+        {
+            first = middle + 1;
+        }
+        else if (arr[middle] == search)
+        {
+            cout << search << " found at location " << middle + 1 << endl;
+            break;
+        }
+        else
+        {
+            last = middle - 1;
+        }
+        middle = (first + last) / 2;
+    }
+    
+    if (first > last)
+    {
+        cout << "Not found! " << search << " is not present in the list.";
+    }
+
+    getch();
+}
